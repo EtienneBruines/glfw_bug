f323a965ba150a35462059306b7dfd4de4eb2d14
Etienne Bruines <etienne@bruines.com>
2016-05-11 02:26:27 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/go-gl/glfw/v3.1/glfw"
+)
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	glfw.WindowHint(glfw.ContextVersionMajor, 2)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.Visible, glfw.False)
+
+	window, err := glfw.CreateWindow(1920, 1200, "Hello Bug", nil, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	window.MakeContextCurrent()
+
+	log.Println("Sleeping to keep window open")
+	time.Sleep(time.Second * 10)
+}
